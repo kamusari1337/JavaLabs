04268f66ab864a6d9d72bcac66e5268cc0e56840
kamusari <ovsyannikovkostyan@gmail.com>
2023-10-13 06:31:57 +0400

    Add increase method

--- src/arrayLabs/ArraysForShoppingCartProject/ShoppingCart.java
+++ src/arrayLabs/ArraysForShoppingCartProject/ShoppingCart.java
@@ -1,21 +1,26 @@
 package arrayLabs.ArraysForShoppingCartProject;
+
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class ShoppingCart {
     private int itemCount; // общее количество товаров в корзине
     private double totalPrice; // общая стоимость товаров в корзине
     private int capacity; // текущая вместимость тележки
+    private ArrayList<Item> cart;
 
     // Создает пустую корзину для покупок, вмещающую 5 товаров.
     public ShoppingCart() {
         capacity = 5;
         itemCount = 0;
         totalPrice = 0.0;
+        cart = new ArrayList<>(capacity);
     }
 
     // добавляет товар в корзину.
     public void addToCart(String itemName, double price, int quantity) {
-        ///напишите код
     }
 
     //Возвращает содержимое корзины вместе с итоговой информацией.
@@ -23,9 +28,9 @@ public class ShoppingCart {
         NumberFormat fmt = NumberFormat.getCurrencyInstance();
         String contents = "\nКорзина\n";
         contents += "\nItem\t\tUnit Price\tQuantity\tTotal\n";
-        for (int i = 0; i < itemCount; i++){
-            //contents += //здесь какой-то код
-        }
+//        for (int i = 0; i < itemCount; i++){
+//            contents += //здесь какой-то код
+//        }
         contents += "\nОбщая цена: " + fmt.format(totalPrice);
         contents += "\n";
         return contents;
@@ -34,6 +39,7 @@ public class ShoppingCart {
     /// Увеличивает вместимость корзины на 3 позиции или
     /// на значение введённое с клавиатуры
     private void increaseSize() {
-        ///напишите код
+        capacity += 3;
+        cart.ensureCapacity(capacity);
     }
 }
