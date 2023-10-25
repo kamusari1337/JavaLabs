645ef10a165c5bceb9680147abc00fcdcc482a2a
kamusari <ovsyannikovkostyan@gmail.com>
2023-10-25 15:09:38 +0400

    Complete sisxth exercise

---
+++ src/queuePractice/TaskSixth.java
@@ -0,0 +1,38 @@
+package queuePractice;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class TaskSixth {
+    public static void main(String[] args) {
+        // Очень странная формулировка задания,
+        // я ее не совсем понял, косо-криво, костыльно,
+        // но сделал
+        Stack<Double> stack1 = new Stack<>();
+        stack1.push(1.0);
+        stack1.push(2.0);
+        stack1.push(3.0);
+
+        Stack<Double> stack2 = new Stack<>();
+        stack2.push(4.0);
+        stack2.push(5.0);
+
+        Queue<Double> queue = new LinkedList<>();
+
+        while (!stack1.isEmpty()) {
+            queue.add(stack1.pop());
+        }
+
+        Stack<Double> tempStack = new Stack<>();
+        while (!stack2.isEmpty()) {
+            tempStack.push(stack2.pop());
+        }
+
+        while(!tempStack.isEmpty()) {
+            queue.add(tempStack.pop());
+        }
+
+        System.out.println(queue.toString());
+    }
+}
